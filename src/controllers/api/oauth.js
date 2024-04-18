5408fbe9063217d6891991249f3cc85e45743c77
zhexun <a41293218@gmail.com>
2024-04-18 23:16:06 +0800

    delete sensitive data

--- src/controllers/api/oauth.js
+++ src/controllers/api/oauth.js
@@ -33,9 +33,9 @@ export const oauth = async (req, res) => {
   const authCode = req.body.authCode; // Assuming authCode is sent in the request body
 
   const oauth2Client = new OAuth2Client({
-      clientId: '65418245861-2e1ofmjjaqe4glkc3q8k8ijducq8qg23.apps.googleusercontent.com',
-      clientSecret: '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',
-      redirectUri: 'http://localhost:3000'
+      clientId: '放你的ClientID',
+      clientSecret: '放你的密鑰',
+      redirectUri: '放你要導向的uri'
   });
 
   try {

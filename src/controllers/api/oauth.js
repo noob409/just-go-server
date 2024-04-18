5278c9986b5c131e2afef8c70695143d2c783d8e
zhexun <a41293218@gmail.com>
2024-04-18 22:59:03 +0800

    revised oauth

--- src/controllers/api/oauth.js
+++ src/controllers/api/oauth.js
@@ -1,29 +1,69 @@
 import { OAuth2Client } from "google-auth-library";
 
-export const oauth = (async (authCode) => {
-    const oauth2Client = new OAuth2Client({
-        clientId: '65418245861-2e1ofmjjaqe4glkc3q8k8ijducq8qg23.apps.googleusercontent.com',
-        clientSecret: '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',
-        redirectUri: 'http://localhost:3000/home'
-      })
+// export const oauth = (async (authCode) => {
+//     const oauth2Client = new OAuth2Client({
+//         clientId: '65418245861-2e1ofmjjaqe4glkc3q8k8ijducq8qg23.apps.googleusercontent.com',
+//         clientSecret: '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',
+//         redirectUri: 'http://localhost:3000/home'
+//       })
     
-      let { tokens } = await oauth2Client.getToken(authCode)
-      client.setCredentials({ access_token: tokens.access_token })
+//       let { tokens } = await oauth2Client.getToken(authCode)
+//       client.setCredentials({ access_token: tokens.access_token })
     
-      const userInfo = await oauth2Client
-        .request({
+//       const userInfo = await oauth2Client
+//         .request({
+//           url: 'https://www.googleapis.com/oauth2/v3/userinfo'
+//         })
+//         .then((response) => response.data)
+//         .catch(() => null)
+    
+//       oauth2Client.revokeCredentials()
+    
+//       return {
+//         id: userInfo.sub,
+//         name: userInfo.name,
+//         avatar: userInfo.picture,
+//         email: userInfo.email,
+//         emailVerified: userInfo.email_verified,
+//       }
+// })
+
+
+export const oauth = async (req, res) => {
+  const authCode = req.body.authCode; // Assuming authCode is sent in the request body
+
+  const oauth2Client = new OAuth2Client({
+      clientId: '65418245861-2e1ofmjjaqe4glkc3q8k8ijducq8qg23.apps.googleusercontent.com',
+      clientSecret: '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',
+      redirectUri: 'http://localhost:3000'
+  });
+
+  try {
+      let { tokens } = await oauth2Client.getToken({ code: authCode });
+      oauth2Client.setCredentials({ access_token: tokens.access_token });
+
+      const userInfoResponse = await oauth2Client.request({
           url: 'https://www.googleapis.com/oauth2/v3/userinfo'
-        })
-        .then((response) => response.data)
-        .catch(() => null)
-    
-      oauth2Client.revokeCredentials()
-    
-      return {
-        id: userInfo.sub,
-        name: userInfo.name,
-        avatar: userInfo.picture,
-        email: userInfo.email,
-        emailVerified: userInfo.email_verified,
+      });
+
+      const userInfo = userInfoResponse.data;
+
+      // Check if userInfo is not null before accessing its properties
+      if (userInfo) {
+          const user = {
+              id: userInfo.sub,
+              name: userInfo.name,
+              avatar: userInfo.picture,
+              email: userInfo.email,
+              emailVerified: userInfo.email_verified,
+          };
+
+          res.status(200).json(user);
+      } else {
+          res.status(500).json({ error: 'Failed to fetch user information' });
       }
-})
+  } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred while processing the request' });
+  }
+};

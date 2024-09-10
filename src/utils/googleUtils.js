8dfeaa2f83b4d8803879f3b7a4527bf5c3526a2c
zhexun <a41293218@gmail.com>
2024-09-10 19:56:17 +0800

    revise validate.js

--- src/utils/googleUtils.js
+++ src/utils/googleUtils.js
@@ -2,9 +2,9 @@ import { OAuth2Client } from 'google-auth-library';
 
 const client = new OAuth2Client(process.env.OAUTH_CLIENT_ID, process.env.OAUTH_CLIENT_SECRET, process.env.OAUTH_REDIRECT_URI);
 
-export const getGoogleInfo = async (token) => {
+export const getGoogleInfo = async (code) => {
   try {
-    let { tokens } = await client.getToken({ code: token });
+    let { tokens } = await client.getToken({ code: code });
     client.setCredentials({ access_token: tokens.access_token });
 
     const response = await client.request({ url: "https://www.googleapis.com/oauth2/v3/userinfo" });

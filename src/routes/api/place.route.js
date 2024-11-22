470a2d9cbefeddd7215cbb4f977bd9337a724d02
flystar0526 <flowermetoer@gmail.com>
2024-11-22 15:48:31 +0800

    add getDetailPlaceById function

--- src/routes/api/place.route.js
+++ src/routes/api/place.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getDetailPlace } from "../../controllers/api/place.controller.js";
-import { validateBody } from "../../middlewares/validateFields.js";
+import {
+  getDetailPlace,
+  getDetailPlaceById,
+} from "../../controllers/api/place.controller.js";
+import { validateParams } from "../../middlewares/validateFields.js";
 
 const PlaceRouter = Router();
 
 PlaceRouter.post("/", validateBody(["googlePlaceIdList"]), getDetailPlace);
+PlaceRouter.get("/:id", validateParams(["id"]), getDetailPlaceById);
 
 export default PlaceRouter;

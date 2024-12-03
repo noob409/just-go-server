fd7c53f2c8782dddabace657b496f1df1e78cddc
xLanStar <danny95624268@gmail.com>
2024-12-03 23:04:19 +0800

    feat: https server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 import "dotenv/config.js";
+import fs, { existsSync } from "fs";
+import https from "https";
 import app from "./app.js";
 import db from "./database.js";
 import logger from "./logger.js";
-import fs from "fs";
-import https from "https";
 
 const { SERVER_HTTPS_PORT, SERVER_HTTP_PORT } = process.env;
-// const options = {
-//   key: fs.readFileSync("src/server.key"),
-//   cert: fs.readFileSync("src/server.crt"),
-// }
 
-// https.createServer(options, app).listen(SERVER_HTTPS_PORT, () => {
-//   logger.info(`HTTPS Server is running on port ${SERVER_HTTPS_PORT}`);
-// });
+if (existsSync("certs/key.pem") && existsSync("certs/cert.crt")) {
+  const options = {
+    key: fs.readFileSync("certs/key.pem"),
+    cert: fs.readFileSync("certs/cert.crt"),
+  };
+
+  https.createServer(options, app).listen(SERVER_HTTPS_PORT, () => {
+    logger.info(`HTTPS Server is running on port ${SERVER_HTTPS_PORT}`);
+  });
+}
 
 app.listen(SERVER_HTTP_PORT, () => {
   logger.info(`HTTP Server is listening on port ${SERVER_HTTP_PORT}`);

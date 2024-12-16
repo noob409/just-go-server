edc412a16870ab23ff37478a23df658b3c1430dd
flystar0526 <flowermetoer@gmail.com>
2024-12-17 00:06:15 +0800

    Update emailUtils.js

--- src/utils/emailUtils.js
+++ src/utils/emailUtils.js
@@ -1,6 +1,6 @@
 import Queue from "bull";
-import nodemailer from "nodemailer";
 import { google } from "googleapis";
+import nodemailer from "nodemailer";
 
 const { OAuth2 } = google.auth;
 
@@ -16,8 +16,7 @@ oauth2Client.setCredentials({
 
 export const emailQueue = new Queue("email", {
   redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    url: process.env.REDIS_URL, // 使用環境變數中設定的 Redis URL
   },
 });
 
@@ -48,11 +47,67 @@ const sendVerificationEmail = async (email, token) => {
       },
     });
 
+    const redirectUrl = `https://just-go-web.onrender.com/verify/${token}`;
+
     const mailOptions = {
       from: process.env.OAUTH_EMAIL,
       to: email,
       subject: "Just Go 帳號驗證",
-      text: `請點擊此連結以驗證您的信箱: \nhttps://just-go-web.onrender.com/verify/${token}`,
+      html: `<div style="height: 100%; width: 100%; background: #f9f9f9; color: #383838">
+      <div style="margin: 0 auto; max-width: 640px; height: 200px"></div>
+      <div
+        style="
+          display: flex;
+          margin: 0 auto;
+          max-width: 640px;
+          background: white;
+          border-radius: 4px;
+          flex-direction: column;
+          gap: 16px;
+          padding: 40px 50px;
+        "
+      >
+        <table style="width: 100%">
+          <tr>
+            <img
+              src="https://just-go-web.onrender.com/logo.png"
+              alt="logo"
+              width="100%"
+            />
+          </tr>
+          <tr>
+            <h1>會員註冊認證說明</h1>
+          </tr>
+          <tr>
+            <p>感謝您註冊 Just Go 會員，請點擊下方按鈕進行認證。</p>
+            <a
+              href="${redirectUrl}"
+              style="
+                padding: 10px 15px;
+                background: #00283d;
+                color: white;
+                font-weight: bold;
+                border-radius: 4px;
+                width: auto;
+                text-decoration: none;
+              "
+            >
+              前往認證
+            </a>
+            <p>
+              若您無法點擊上方按鈕，請複製以下連結至瀏覽器開啟：
+              <a href="${redirectUrl}">${redirectUrl}</a>
+              <br />
+              此信件由系統自動發送，請勿回覆。如有任何問題，請聯絡客服。
+            </p>
+          </tr>
+          <tr>
+            <p>Just Go 團隊 敬上</p>
+          </tr>
+        </table>
+      </div>
+      <div style="margin: 0 auto; max-width: 640px; height: 200px"></div>
+    </div>`,
     };
 
     await transporter.sendMail(mailOptions);

ceab6ba73ece780514588ea2e62fbfd2411137c4
flystar0526 <flowermetoer@gmail.com>
2024-12-16 23:45:35 +0800

    Update redis url

--- src/utils/placeUtils.js
+++ src/utils/placeUtils.js
@@ -1,8 +1,10 @@
-import redis from 'redis';
+import redis from "redis";
 
 const EXPIRATION_TIME = 86400; // 24 hours
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
+});
 
 redisClient.connect();
 
@@ -11,15 +13,15 @@ export const getCache = async (key) => {
     const data = await redisClient.get(key);
     return data ? JSON.parse(data) : null;
   } catch (error) {
-    console.error('Redis get error:', error);
+    console.error("Redis get error:", error);
     return null;
   }
-}
+};
 
 export const setCache = async (key, data) => {
   try {
     await redisClient.setEx(key, EXPIRATION_TIME, JSON.stringify(data));
   } catch (error) {
-    console.error('Redis set error:', error);
+    console.error("Redis set error:", error);
   }
-}
+};
